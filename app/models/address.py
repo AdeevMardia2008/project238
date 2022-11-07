e102ce392a6180f75c6f53e421f07dadbaf6ef26
AdeevMardia2008 <71126451+AdeevMardia2008@users.noreply.github.com>
2022-11-07 20:56:44 +0530

    Add files via upload

---
+++ app/models/address.py
@@ -0,0 +1,38 @@
+from app import db
+import uuid
+
+#creating a class to store addresses
+class Address(db.Model):
+    __tablename__ = "address"
+    id = db.Column(db.Integer, primary_key=True)
+    guid = db.Column(db.String, nullable=False, unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    house_number = db.Column(db.String)
+    city = db.Column(db.String)
+    state = db.Column(db.String)
+    country = db.Column(db.String)
+    pin_code = db.Column(db.String)
+
+    @staticmethod
+
+    #storing the information
+    def create(user_id, house_number, city, state, country, pin_code):
+        address_dict = dict(
+            guid = str(uuid.uuid4()),
+            user_id = user_id,
+            house_number = house_number,
+            city = city,
+            state = state,
+            country = country,
+            pin_code = pin_code
+        )
+        address_obj = Address(**address_dict)
+        db.session.add(address_obj)
+        db.session.commit()
+
+
+    #updating the information
+    def update(self, **details_dict):
+        for k,v in details_dict.items():
+            setattr(self, k, v)
+        db.session.commit()
